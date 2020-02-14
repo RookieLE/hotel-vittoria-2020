8958267a095fb70f0bb70458f83a6bda0f8b1e55
Leonardo Tononi <easaaa@protonmail.com>
2020-02-14 20:22:07 +0100

    Removed layout plagin - fix err button style ios

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  plugins: [
-    `gatsby-plugin-smoothscroll`,
-    `gatsby-plugin-sass`,
-    `gatsby-plugin-layout`,
-  ],
+  plugins: [`gatsby-plugin-smoothscroll`, `gatsby-plugin-sass`],
   pathPrefix: "easaaa.github.io/hotel-vittoria-2020/",
 }
